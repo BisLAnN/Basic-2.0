f54f2279591373f99debb1c13fa2c28105c8c799
BisLAnN <huadebislan23@gmail.com>
2022-09-19 20:27:26 +0300

    first commit

---
+++ Basic/Core/UserParser.cs
@@ -0,0 +1,24 @@
+﻿using Basic.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Core
+{
+    public static class UserParser
+    {
+        public static IEnumerable<User> ParseFile(string path)
+        {
+            foreach (string line in File.ReadAllLines("users1.txt").Skip(1))
+            {
+                string[] props = line.Split(';');
+
+                yield return new User(props[0], props[1], props[2],
+                    Convert.ToInt32(props[3]), Convert.ToDecimal(props[4]));
+            }
+        }
+    }
+}
---
+++ Basic/Model/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Model
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+
+        public User()
+        {
+            FirstName = string.Empty;
+            MiddleName = string.Empty;
+            LastName = string.Empty;
+            Age = int.MinValue;
+            Salary = decimal.MinValue;
+        }
+
+        public User(string firstName, string middleName, string lastName, int age,
+            decimal salary)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+
+        public override string ToString() => $"ФИО: {FirstName} {MiddleName} {LastName}\n" +
+            $"Дата рождения: {Age}\n" + $"Зарплата:{Salary}";
+    }
+}
